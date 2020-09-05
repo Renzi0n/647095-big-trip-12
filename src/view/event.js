a828a3e0e1443b9b52980a841e95372ca45570ff
Andrew <andersen7298@gmail.com>
2020-09-05 20:37:53 +0300

    Исправлен баг с отображением иконок с типом

--- src/view/event.js
+++ src/view/event.js
@@ -31,7 +31,7 @@ export const createEventTemplate = (eventsData) => {
     `<li class="trip-events__item">
       <div class="event">
         <div class="event__type">
-          <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event ${type} icon">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event ${type} icon">
         </div>
         <h3 class="event__title">${type} ${generateSuffix(type)} ${city}</h3>
 
