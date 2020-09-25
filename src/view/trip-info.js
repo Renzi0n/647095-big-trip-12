eca47bc0ee6a1a8dc3ec8672bfd5cb42f8538b3c
Andrew <andersen7298@gmail.com>
2020-09-25 22:28:54 +0300

    Реализованы информация о поездке

--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -1,23 +1,52 @@
 import AbstractView from './abstract.js';
+import {getTripTitle, getTripDates, getTripPrice} from '../utils/trip-info.js';
 
-const createTripInfoTemplate = () => {
+const createTripInfoTemplate = (events) => {
+  let title = ``;
+  let dates = ``;
+  let price = ``;
+
+  if (events.length) {
+    title = getTripTitle(events);
+    dates = getTripDates(events);
+    price = getTripPrice(events);
+  }
+
   return (
     `<section class="trip-main__trip-info  trip-info">
       <div class="trip-info__main">
-        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+        <h1 class="trip-info__title">${title}</h1>
 
-        <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+        <p class="trip-info__dates">${dates}</p>
       </div>
 
       <p class="trip-info__cost">
-        Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+        Total: &euro;&nbsp;<span class="trip-info__cost-value">${price}</span>
       </p>
     </section>`
   );
 };
 
 export default class TripInfo extends AbstractView {
+  constructor(events) {
+    super();
+
+    this._events = events;
+  }
+
   getTemplate() {
-    return createTripInfoTemplate();
+    return createTripInfoTemplate(this._events);
+  }
+
+  updateTripInfo(events) {
+    this._events = events;
+
+    let prevElement = this.getElement();
+    this.removeElement();
+
+    const newElement = this.getElement();
+
+    prevElement.replaceWith(newElement);
+    prevElement = null;
   }
 }
